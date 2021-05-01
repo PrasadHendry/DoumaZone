51e0619cb2f5f51e87f9200160c4b7fc1a3a9ff8
Sid <siddharth.shyampur@gmail.com>
2021-05-01 16:58:49 +0530

    Test commit.

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace DoumaZone
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new guest_page());
+            Application.Run(new admin_page());
         }
     }
 }
--- admin_page.cs
+++ admin_page.cs
@@ -5,11 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace DoumaZone
 {
     public partial class admin_page : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\siddh\Documents\DoumaDB.mdf;Integrated Security=True;Connect Timeout=30");
+
         public admin_page()
         {
             InitializeComponent();
@@ -20,6 +23,50 @@ namespace DoumaZone
             guest_page gp = new guest_page();
             gp.Show();
             this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from event_co_acc";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+
+            catch(Exception ex)
+            {
+                MessageBox.Show("Unable to display the table.");
+            }
+
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "insert into event_co_acc values ( '" + Convert.ToInt32(textBox1.Text) + "', '" + textBox2.Text + "', '" + Convert.ToInt32(textBox3.Text) + "', '" + textBox4.Text + "', '" + Convert.ToInt32(textBox5.Text) + "')";
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Event added successfully.");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to add the event.");
+                MessageBox.Show(ex.ToString());
+            }
+
+            con.Close();
         }
     }
 }
