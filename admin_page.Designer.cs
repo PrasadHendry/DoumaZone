3f5a702fd69e4df69758b3b847451f8eb6f7a862
Prasad_Student <el.verethragna@gmail.com>
2021-05-04 16:25:00 +0530

    Nothing

--- admin_page.Designer.cs
+++ admin_page.Designer.cs
@@ -54,10 +54,9 @@ namespace DoumaZone
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1302, 21);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Location = new System.Drawing.Point(1488, 28);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(119, 27);
+            this.button1.Size = new System.Drawing.Size(136, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Logout";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,27 +64,25 @@ namespace DoumaZone
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(117, 168);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox5.Location = new System.Drawing.Point(134, 224);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(146, 23);
+            this.textBox5.Size = new System.Drawing.Size(166, 27);
             this.textBox5.TabIndex = 16;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(15, 144);
+            this.label7.Location = new System.Drawing.Point(17, 192);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(74, 15);
+            this.label7.Size = new System.Drawing.Size(92, 20);
             this.label7.TabIndex = 15;
             this.label7.Text = "Event Name:";
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(238, 223);
-            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Location = new System.Drawing.Point(272, 297);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(66, 26);
+            this.button5.Size = new System.Drawing.Size(75, 35);
             this.button5.TabIndex = 14;
             this.button5.Text = "Search";
             this.button5.UseVisualStyleBackColor = true;
@@ -93,10 +90,9 @@ namespace DoumaZone
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(156, 223);
-            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Location = new System.Drawing.Point(178, 297);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(76, 26);
+            this.button4.Size = new System.Drawing.Size(87, 35);
             this.button4.TabIndex = 13;
             this.button4.Text = "Delete";
             this.button4.UseVisualStyleBackColor = true;
@@ -104,10 +100,9 @@ namespace DoumaZone
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(84, 223);
-            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Location = new System.Drawing.Point(96, 297);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(66, 26);
+            this.button3.Size = new System.Drawing.Size(75, 35);
             this.button3.TabIndex = 12;
             this.button3.Text = "Update";
             this.button3.UseVisualStyleBackColor = true;
@@ -115,10 +110,9 @@ namespace DoumaZone
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 223);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Location = new System.Drawing.Point(14, 297);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(66, 26);
+            this.button2.Size = new System.Drawing.Size(75, 35);
             this.button2.TabIndex = 11;
             this.button2.Text = "Add";
             this.button2.UseVisualStyleBackColor = true;
@@ -126,113 +120,110 @@ namespace DoumaZone
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(117, 141);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox4.Location = new System.Drawing.Point(134, 188);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(146, 23);
+            this.textBox4.Size = new System.Drawing.Size(166, 27);
             this.textBox4.TabIndex = 10;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(117, 113);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox3.Location = new System.Drawing.Point(134, 151);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(146, 23);
+            this.textBox3.Size = new System.Drawing.Size(166, 27);
             this.textBox3.TabIndex = 9;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(117, 85);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Location = new System.Drawing.Point(134, 113);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(146, 23);
+            this.textBox2.Size = new System.Drawing.Size(166, 27);
             this.textBox2.TabIndex = 8;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(117, 57);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(134, 76);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(146, 23);
+            this.textBox1.Size = new System.Drawing.Size(166, 27);
             this.textBox1.TabIndex = 7;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(17, 171);
+            this.label5.Location = new System.Drawing.Point(19, 228);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.Size = new System.Drawing.Size(117, 20);
             this.label5.TabIndex = 6;
             this.label5.Text = "Phone No (+91):";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 116);
+            this.label4.Location = new System.Drawing.Point(17, 155);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.Size = new System.Drawing.Size(67, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Event ID:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(17, 88);
+            this.label3.Location = new System.Drawing.Point(19, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 15);
+            this.label3.Size = new System.Drawing.Size(52, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Name:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 60);
+            this.label2.Location = new System.Drawing.Point(17, 80);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.Size = new System.Drawing.Size(96, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Event Co. ID: ";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(14, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(103, 15);
+            this.label1.Size = new System.Drawing.Size(128, 20);
             this.label1.TabIndex = 2;
             this.label1.Text = "Manage Event Co.";
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.dataGridView1);
-            this.panel2.Location = new System.Drawing.Point(421, 37);
-            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Location = new System.Drawing.Point(481, 49);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(827, 213);
+            this.panel2.Size = new System.Drawing.Size(945, 284);
             this.panel2.TabIndex = 2;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(827, 213);
+            this.dataGridView1.Size = new System.Drawing.Size(945, 284);
             this.dataGridView1.TabIndex = 0;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(421, 9);
+            this.label6.Location = new System.Drawing.Point(481, 12);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(84, 15);
+            this.label6.Size = new System.Drawing.Size(103, 20);
             this.label6.TabIndex = 3;
             this.label6.Text = "Event Co. Info:";
             // 
             // admin_page
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1451, 633);
+            this.ClientSize = new System.Drawing.Size(1658, 844);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -251,9 +242,9 @@ namespace DoumaZone
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label5);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "admin_page";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "admin_page";
             this.Load += new System.EventHandler(this.admin_page_Load);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
