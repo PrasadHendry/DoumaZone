47b70197844859e2fb57ad56eb0f4582b38699bd
Prasad_Student <el.verethragna@gmail.com>
2021-05-13 19:29:46 +0530

    icon added

--- welcome_page.Designer.cs
+++ welcome_page.Designer.cs
@@ -146,6 +146,7 @@ namespace DoumaZone
             this.Controls.Add(this.radioButton1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
